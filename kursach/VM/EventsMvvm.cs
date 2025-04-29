085744e17aae75d13ba90ab6bc8e789955973178
user <user@CAB1-07>
2025-04-29 13:08:42 +1000

    29.04dfsddsc

--- kursach/VM/EventsMvvm.cs
+++ kursach/VM/EventsMvvm.cs
@@ -10,77 +10,77 @@ namespace kursach.VM
 {
     internal class EventsMvvm : BaseVM
     {
-        private Event newEvent = new();
+        //private Event newEvent = new();
 
-        public Event NewEvent
-        {
-            get => newEvent;
-            set
-            {
-                newEvent = value;
-                Signal();
-            }
-        }
+        //public Event NewEvent
+        //{
+        //    get => newEvent;
+        //    set
+        //    {
+        //        newEvent = value;
+        //        Signal();
+        //    }
+        //}
 
-        private Event selectedEvent = new();
+        //private Event selectedEvent = new();
 
-        public Event SelectedEvent
-        {
-            get => selectedEvent;
-            set
-            {
-                selectedEvent = value;
-                Signal();
-            }
-        }
+        //public Event SelectedEvent
+        //{
+        //    get => selectedEvent;
+        //    set
+        //    {
+        //        selectedEvent = value;
+        //        Signal();
+        //    }
+        //}
 
-        private ObservableCollection<Event> events = new();
+        //private ObservableCollection<Event> events = new();
 
-        public ObservableCollection<Event> Events
-        {
-            get => events;
-            set
-            {
-                events = value;
-                Signal();
-            }
-        }
+        //public ObservableCollection<Event> Events
+        //{
+        //    get => events;
+        //    set
+        //    {
+        //        events = value;
+        //        Signal();
+        //    }
+        //}
 
 
-        public CommandMvvm InsertEvent { get; set; }
+        //public CommandMvvm InsertEvent { get; set; }
 
-        public CommandMvvm NextPage { get; set; }
-        public EventsMvvm()
-        {
-            SelectAll();
-            InsertEvent = new CommandMvvm(() =>
-            {
-                ClientDB.GetDb().Insert(NewClient);
-                NewClient = new();
-                SelectAll();
-            },
-                () =>
-                !string.IsNullOrEmpty(newClient.FullName) &&
-                !string.IsNullOrEmpty(newClient.Phone) &&
-                !string.IsNullOrEmpty(newClient.Email) &&
-                !string.IsNullOrEmpty(newClient.Notes));
+        //public CommandMvvm NextPage { get; set; }
+        //public EventsMvvm()
+        //{
+        //    SelectAll();
+        //    InsertEvent = new CommandMvvm(() =>
+        //    {
+        //        ClientDB.GetDb().Insert(NewClient);
+        //        NewClient = new();
+        //        SelectAll();
+        //    },
+        //        () =>
+        //        !string.IsNullOrEmpty(newClient.FullName) &&
+        //        !string.IsNullOrEmpty(newClient.Phone) &&
+        //        !string.IsNullOrEmpty(newClient.Email) &&
+        //        !string.IsNullOrEmpty(newClient.Notes));
 
-            NextPage = new CommandMvvm(() => {
-                Events events = new Events(SelectedClient);
-                events.Show();
-                close?.Invoke();
-            },
-                () => SelectedClient != null);
-        }
+        //    NextPage = new CommandMvvm(() => {
+        //        Events events = new Events(SelectedClient);
+        //        events.Show();
+        //        close?.Invoke();
+        //    },
+        //        () => SelectedClient != null);
+        //}
 
-        private void SelectAll()
-        {
-            Clients = new ObservableCollection<Client>(ClientDB.GetDb().SelectAll());
-        }
-        Action close;
-        internal void SetClose(Action close)
-        {
-            this.close = close;
-        }
+        //private void SelectAll()
+        //{
+        //    Clients = new ObservableCollection<Client>(ClientDB.GetDb().SelectAll());
+        //}
+        //Action close;
+        //internal void SetClose(Action close)
+        //{
+        //    this.close = close;
+        //}
     }
 }
